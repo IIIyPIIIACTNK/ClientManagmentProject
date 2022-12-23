9a117b608dd88592e98aeedeecdb59a49c66351c
Alexey Shustrov <humoressus@yandex.ru>
2022-12-23 18:38:33 +0300

    universal path names static class Repository ClientObject IComparable realisation

---
+++ ClientManagmentProject/Windows/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace ClientManagmentProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void StartButton(object sender, RoutedEventArgs e)
+        {
+
+            switch (ClientType.SelectedIndex)
+            {
+                case 0:
+                    var managerWindow = new ManagerWindow();
+                    Close();
+                    managerWindow.Show();
+                    break;
+                case 1:
+                    var consultantWindow = new ConsultantWindow();
+                    Close();
+                    consultantWindow.Show();
+                    break;
+                default:
+                    MessageBox.Show("Выберите тип клиента", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+            }
+        }
+    }
+}
---
+++ ClientManagmentProject/Windows/ManagerWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClientManagmentProject
+{
+
+    public partial class ManagerWindow : Window
+    {
+        Manager manager;
+
+        public ManagerWindow()
+        {
+            InitializeComponent();
+            manager = new Manager();
+            clientsList.ItemsSource = Repository.ObsClients;
+        }
+
+        private void clientsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            clientPhoneNumber.Text = Repository.Clients[clientsList.SelectedIndex].PhoneNumber;
+            clientIdNumber.Text = Repository.Clients[clientsList.SelectedIndex].IdNumber;
+
+            clientsChangesList.ItemsSource = Repository.GetClientChangesList(clientsList.SelectedIndex);
+        }
+
+        private void ChangeButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (clientsList.SelectedItem == null)
+            {
+                MessageBox.Show("Необходимо выбрать клиента для изменения", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (clientPhoneNumber.Text == string.Empty || clientIdNumber.Text == string.Empty || clientName.Text == string.Empty)
+            {
+                clientPhoneNumber.Text = Repository.Clients[clientsList.SelectedIndex].PhoneNumber;
+                clientIdNumber.Text = Repository.Clients[clientsList.SelectedIndex].IdNumber;
+                clientName.Text = string.Empty;
+                MessageBox.Show("Поля необходимо заполнить", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            manager.SetPhoneNumber(Repository.Clients[clientsList.SelectedIndex], clientPhoneNumber.Text);
+            manager.SetIdNumber(Repository.Clients[clientsList.SelectedIndex], clientIdNumber.Text);
+            manager.SetClientName(Repository.Clients[clientsList.SelectedIndex], clientName.Text);
+        }
+
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Repository.SaveRepository();
+            var mainWindow = new MainWindow();
+            Close();
+            mainWindow.Show();
+
+        }
+
+        private void AddClientButtonClick(object sender, RoutedEventArgs e)
+        {
+            var addClientWindow = new AddClientWindow();
+            addClientWindow.Show();
+        }
+
+        private void SaveClientButtonClick(object sender, RoutedEventArgs e)
+        {
+            Repository.SaveRepository();
+            MessageBox.Show("Сохранено");
+        }
+
+
+    }
+}
